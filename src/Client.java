544a1fce6f1c99f3eb5e86e7cf8663e9a72c0cd8
Dmitriy Zadorozhnyy <zdmitrij@yandex.ru>
2023-06-29 21:06:37 +0300

    2023-06-29 клиент соединяетя с яндексом

---
+++ src/Client.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+
+
+
+        try (
+                Socket socket = new Socket("www.yandex.ru", 80);
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                BufferedReader reader = new BufferedReader((new InputStreamReader(socket.getInputStream())));
+        ) {
+            System.out.println("Connected to server");
+            String request = "GET";
+            System.out.println("Request - " + request);
+            writer.write(request);
+            writer.newLine();
+            writer.flush();
+
+            String response = reader.readLine();
+            System.out.println("Response " + response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
